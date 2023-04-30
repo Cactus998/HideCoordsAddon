34e618be4e6422f9fd39fe9183732e438d3f4aff
Cactus998 <108695149+Cactus998@users.noreply.github.com>
2023-04-30 11:06:20 +0300

    Add files via upload

---
+++ src/main/java/me/cactus/Main.java
@@ -0,0 +1,154 @@
+package me.cactus;
+
+import fr.xephi.authme.events.LoginEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Main extends JavaPlugin implements Listener {
+    private FileConfiguration config;
+
+    @Override
+    public void onEnable() {
+        Plugin authme = Bukkit.getPluginManager().getPlugin("AuthMe");
+        if (authme == null) {
+            getLogger().warning("AuthMe не найден!");
+            Bukkit.getPluginManager().disablePlugin(this);
+        } else {
+            getLogger().info("Плагин AuthMe найден. Всё работает.");
+        }
+        Bukkit.getPluginManager().registerEvents(this, this);
+        config = getConfig();
+        config.addDefault("world-name", "world");
+        config.addDefault("spawn-x", 0);
+        config.addDefault("spawn-y", 10000);
+        config.addDefault("spawn-z", 0);
+        config.options().copyDefaults(true);
+        saveConfig();
+    }
+
+    private final Map<UUID, Location> playerLocations = new HashMap<>();
+    private final Map<UUID, GameMode> playerGamemode = new HashMap<>();
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+        Location location = player.getLocation();
+        GameMode gamemode = player.getGameMode();
+        teleport(player);
+        player.setGameMode(GameMode.SPECTATOR);
+
+        if (!playerLocations.containsKey(uuid)) {
+            playerLocations.put(uuid, location);
+        }
+
+        if (!playerGamemode.containsKey(uuid)) {
+            playerGamemode.put(uuid, gamemode);
+        }
+    }
+
+    private void teleport(Player player) {
+        String worldName = config.getString("world-name");
+        double spawnX = config.getDouble("spawn-x");
+        double spawnY = config.getDouble("spawn-y");
+        double spawnZ = config.getDouble("spawn-z");
+        World world = Bukkit.getWorld(worldName);
+        player.teleport(new Location(world, spawnX, spawnY, spawnZ));
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (player.getGameMode() == GameMode.SPECTATOR) {
+            teleport(player);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        if (event.getCause() == PlayerTeleportEvent.TeleportCause.SPECTATE) {
+            Player player = event.getPlayer();
+            if (player.getGameMode() == GameMode.SPECTATOR) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onLogin(LoginEvent event) {
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+        player.setNoDamageTicks(60);
+        Location location = playerLocations.get(uuid);
+        GameMode gameMode = playerGamemode.get(uuid);
+        player.teleport(location);
+        playerLocations.remove(uuid);
+        player.setGameMode(gameMode);
+        playerGamemode.remove(uuid);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+
+        if (playerLocations.containsKey(uuid) && playerGamemode.containsKey(uuid)) {
+            Location location = playerLocations.get(uuid);
+            GameMode gameMode = playerGamemode.get(uuid);
+            player.teleport(location);
+            playerLocations.remove(uuid);
+            player.setGameMode(gameMode);
+            playerGamemode.remove(uuid);
+        }
+    }
+
+    @EventHandler
+    public void onPluginDisable(PluginDisableEvent event) {
+        // TODO: доделать проверку на имеено использование релоад
+        if (event.getPlugin().equals(this)) {
+//            getLogger().severe("==================================================");
+//            getLogger().severe("Использование /reload может навредить серверу.");
+//            getLogger().severe("Из-за несохранённых данных сервер будет выключен.");
+//            getLogger().severe("==================================================");
+            for (Map.Entry<UUID, Location> entry : playerLocations.entrySet()) {
+                UUID uuid = entry.getKey();
+                Location location = entry.getValue();
+                Player player = getPlayer(uuid);
+                player.teleport(location);
+            }
+            for (Map.Entry<UUID, GameMode> entry : playerGamemode.entrySet()) {
+                UUID uuid = entry.getKey();
+                GameMode gameMode = entry.getValue();
+                Player player = getPlayer(uuid);
+                player.setGameMode(gameMode);
+            }
+            Bukkit.shutdown();
+        }
+    }
+
+    public Player getPlayer(UUID uuid) {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player.getUniqueId().equals(uuid)) {
+                return player;
+            }
+        }
+        return null;
+    }
+}
